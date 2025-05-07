5a27ad8d4eeef5e261d81fa7c70579581eb5ce34
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-05-07 10:54:58 +0530

    hi devops 07

---
+++ j/src/Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a, b, result1, result2;
+        a = 10;
+        b = 5;
+        result1 = a + b;
+        result2 = a - b;
+        System.out.println("The Sum Of Two Numbers is :" + result1);
+        System.out.println("The Sub Of Two Numbers is :" + result2);
+    }
+}
